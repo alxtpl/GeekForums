143bebf36dd19f199883d1e4d773175d6dad4ea6
alxtpl <alxtpl@gmai.com>
2021-03-01 22:36:53 +0200

    ver

---
+++ GeekForums.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GeekForums.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekForums.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
+        public DbSet<Forum> Forums  { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostReplies  { get; set; }
+
+    }
+}
---
+++ GeekForums.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GeekForums.Data
+{
+    public class Class1
+    {
+    }
+}
