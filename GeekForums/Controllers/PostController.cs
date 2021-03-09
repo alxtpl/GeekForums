6b0c2e11de9081fdfda52e2b5feee6f0293463a7
alxtpl <alxtpl@gmai.com>
2021-03-09 16:38:30 +0200

    Create Post Create Form

--- GeekForums/Controllers/PostController.cs
+++ GeekForums/Controllers/PostController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using GeekForums.Data;
 using GeekForums.Data.Models;
 using GeekForums.Models.Post;
 using GeekForums.Models.Post.Reply;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekForums.Controllers
@@ -12,9 +14,15 @@ namespace GeekForums.Controllers
     public class PostController : Controller
     {
         private readonly IPost _postService;
-        public PostController(IPost postService)
+        private readonly IForum _forumService;
+
+        private static UserManager<ApplicationUser> _userManager;
+
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser> userManager)
         {
             _postService = postService;
+            _forumService = forumService;
+            _userManager = userManager;
         }
 
         public IActionResult Index(int id)   
@@ -38,6 +46,47 @@ namespace GeekForums.Controllers
             };
 
             return View(model);
+        }
+
+        public IActionResult Create(int id)
+        {
+            //Note id  is Forum.Id
+            var forum = _forumService.GetById(id);
+            var model = new NewPostModel
+            {
+                 ForumName = forum.Title,
+                 ForumId = forum.Id,
+                 ForumImageUrl = forum.ImageUrl,
+                 AuthorName =   User.Identity.Name 
+
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = _userManager.FindByIdAsync(userId).Result;
+            var post = BuildPost(model, user);
+
+            await _postService.Add(post);
+
+            // TODO :   Implement user rating Managment
+
+            return RedirectToAction("Index", "Post", new { id = post.Id });
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            var forum = _forumService.GetById(model.ForumId);
+            return new Post
+            {
+               Title = model.Title,
+               Content = model.Content,
+               Created = DateTime.Now,
+               User = user,
+               Forum = forum
+            };
         }
 
         private  IEnumerable <PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
